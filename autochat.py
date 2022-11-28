fb94eb8efa89a69f384caebfe4d8dad03788527d
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-11-28 01:21:47 +0100

    autochat

---
+++ autochat.py
@@ -0,0 +1,35 @@
+import pyautogui
+import time
+
+
+
+# while True:
+#  text= "Gladness is a super awesome human, much more awesome than many other people😌😌😌😌 "
+def text(text):
+ time.sleep(1)
+ pyautogui.typewrite(text)
+ time.sleep(1)
+ pyautogui.press('enter')
+
+text("ok Baby girl")
+text(" am in a serious mode now thank god you not online now meaning youll get to see all this later")
+text( "get a chair and sit before yoou continue" )
+text("so there somthing ive been wanting to tell you for a while now")
+text( "its been pressing upon my heart for a while now" )
+text( "like i cant spend a day without thinking about how to tell you since it crossed my mind" )
+text(  "the issue is i really dont know how to tell you ")
+text( "i know you have the sprit of God and you be able to tell if am speaking the truth" )
+text( "Gladdy hmm ok")
+text( "let me just say it how it is " )
+text( "but first of all i dont want you to take it wrongly" )
+text( "and m ill really need you to give me  your honest opinion" )
+text( "ok i know weve know each other for a while now" )
+text( "i think 4 yrs although we got to know better last year ending " )
+text( "ok here is my point... pls dont act weirdly" )
+text("i know youll get where i am comming from or heading to by no")
+text("but just to be clear let me say it")
+text( "hmmm" )
+text( "gladness i was thinking..." )
+text( "what exactly do you think am about to tell you" )
+
+text("i just hoped you didnt start from the bottom ")
